d82a6f902e9ddbbe6b6a040f6fc0d4546585d05e
michal <michal@michals-MacBook-Pro.local>
2021-02-28 13:35:16 +0100

    Change fetch questionnaires to use redux

--- client/reducers/root-reducer.js
+++ client/reducers/root-reducer.js
@@ -1,21 +1,28 @@
 import { createStore, applyMiddleware } from 'redux'
+import { createAction } from 'redux-actions'
 import createSagaMiddleware from 'redux-saga'
 import rootSaga from "../sagas/root-saga";
 
+export const FETCH_QUESTIONNAIRES = createAction('QUESTIONNAIRES/FETCH-QUESIONNAIRES');
+export const FETCH_QUESTIONNAIRES_SUECCES = createAction('FETCH_QUESTIONNAIRES_SUECCES');
+
+
 const sagaMiddleware = createSagaMiddleware()
-function counterReducer(state = { value: 2 }, action) {
+function rootReducer(state = { value: 2, questionnaires:[] }, action) {
   switch (action.type) {
     case 'counter/incremented':
       return { value: state.value + 1 }
     case 'counter/decremented':
       return { value: state.value - 1 }
+    case 'FETCH_QUESTIONNAIRES_SUECCES':
+      return {...state, questionnaires: action.payload }
     default:
       return state
   }
 }
 
 const store = createStore(
-  counterReducer,
+  rootReducer,
   applyMiddleware(sagaMiddleware)
 )
 sagaMiddleware.run(rootSaga);
--- client/sagas/root-saga.js
+++ client/sagas/root-saga.js
@@ -1,8 +1,9 @@
 import { all } from 'redux-saga/effects'
+import FetchQuestionnaires from './fetch-questionnaires'
 import IncrementAsync from './increment-sage'
-
 export default function* rootSaga() {
   yield all([
-    IncrementAsync()
+    IncrementAsync(),
+    FetchQuestionnaires(),
   ])
 }
