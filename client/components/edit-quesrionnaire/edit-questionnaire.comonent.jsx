30e4ec063da4e0446c4a0d6a30554f3f8c58f075
michal <michal@michals-MacBook-Pro.local>
2021-03-30 13:20:41 +0200

    Code refacotr

--- client/components/edit-quesrionnaire/edit-questionnaire.comonent.jsx
+++ client/components/edit-quesrionnaire/edit-questionnaire.comonent.jsx
@@ -1,55 +1,46 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import EditQuestion from '../edit-question/edit-question.component';
 import "./edit-questionnaire.styles.css"
 import {connect} from 'react-redux';
 import { CREATE_QUESTION, FETCH_QUESTIONS } from '../../reducers/root-reducer';
 
-class EditQuestionnaire extends React.Component{
-  constructor(props){
-    super(props);
-  }
-
-  componentDidMount(){
-    const questionnaire_id = this.props.match.params.id
-    this.props.FetchQuestions(questionnaire_id)
-  }
-
-  handleQuesionComponentClick = (index) => {
-    const questions = this.props.questions.map(q => q.id )
+const EditQuestionnaire = (props) => {
+  useEffect(() => { 
+    const questionnaire_id = props.match.params.id;
+    props.FetchQuestions(questionnaire_id)
+   }, []);
+  const handleQuesionComponentClick = (index) => {
+    const questions = props.questions.map(q => q.id )
     if (questions.length === (index+1)){
-      const questionnaire_id = this.props.match.params.id
-      this.props.createQuestion(questionnaire_id);
+      const questionnaire_id = props.match.params.id
+      props.createQuestion(questionnaire_id);
     }
   }
-
-  render(){
-    const { questions } = this.props
-    const sortedQuestions = questions.sort((a, b) => (a.id > b.id) ? 1 : -1);
-    return(
-      <div>
-      <section className="jumbotron jumbotron-fluid text-center">
-            <div className="container py-5">
-              <h1 className="display-4">Your Questionnaire</h1>
-            </div>
-      </section>
-      <div className="center w-60">      
-        {sortedQuestions.map( (item,index) => (
-          <EditQuestion 
-            handleQuesionComponentClick={this.handleQuesionComponentClick} 
-            index={index}
-            key={item.id}
-            id={item.id}
-            content={item.content}
-            answers={item.answers}
-            match={this.props.match}
-            lastAnswer={item.answers[item.answers.length -1 ]}
-
-          />
-        ))}
-      </div>
+  const { questions } = props
+  const sortedQuestions = questions.sort((a, b) => (a.id > b.id) ? 1 : -1);
+  return(
+  <div>
+    <section className="jumbotron jumbotron-fluid text-center">
+          <div className="container py-5">
+            <h1 className="display-4">Your Questionnaire</h1>
+          </div>
+    </section>
+    <div className="center w-60">      
+      {sortedQuestions.map( (item,index) => (
+        <EditQuestion 
+          handleQuesionComponentClick={handleQuesionComponentClick} 
+          index={index}
+          key={item.id}
+          id={item.id}
+          content={item.content}
+          answers={item.answers}
+          match={props.match}
+          lastAnswer={item.answers[item.answers.length -1 ]}
+        />
+      ))}
     </div>
-    )
-  }
+  </div>
+  )
 }
 
 const mapDispatchToProps = dispatch => {
--- client/components/edit-question/edit-question.component.jsx
+++ client/components/edit-question/edit-question.component.jsx
@@ -4,73 +4,67 @@ import headers from '../../utilis/apiHeader';
 import EditQuestionAnswear from '../edit-answear/edit-answear.component';
 import { connect } from 'react-redux';
 
-class EditQuestion extends React.Component {
-  
+const EditQuestion = (props) => {
+  const { handleQuesionComponentClick, index } = props; 
+  const answers = props.answers.sort((a, b) => (a.id > b.id) ? 1 : -1);
 
-  handleAddAnswear = () => {
-    const questionnaire_id = this.props.match.params.id
+  const handleAddAnswear = () => {
+    const questionnaire_id = props.match.params.id
     const body = {
-      questionId: this.props.id,
+      questionId: props.id,
       questionnaire_id: questionnaire_id,
     };
-    this.props.addAnswer(body);
+    props.addAnswer(body);
   }
 
-  handleUpdateQuestion = () => {
-    const questionnaire_id = this.props.match.params.id
+  const handleUpdateQuestion = () => {
+    const questionnaire_id = props.match.params.id
     const body = {
-      content: this.props.content,
-      answers: this.props.answers,
-      questionId: this.props.id,
+      content: props.content,
+      answers: props.answers,
+      questionId: props.id,
       questionnaire_id: questionnaire_id,
     };
-    this.props.updateQuestion(body)
+    props.updateQuestion(body)
   }
-
-  render(){
-    const { handleQuesionComponentClick, index } = this.props; 
-    const answers = this.props.answers.sort((a, b) => (a.id > b.id) ? 1 : -1);
-    return (
-      <div 
-        className="fl w-100 bg-light-green2 bt bw3 border-dark-green2 mt4"
-        onClick={() => handleQuesionComponentClick(index)}
-        onBlur={()=> this.handleUpdateQuestion()}
-      >
-        <div className="fl f5 code pa2 tr pl2 mt3">
-          {index + 1}.
-        </div>
-        <div className="center w-90 relative mt3">
-          <input
-            name='question'
-            type='text'
-            placeholder=" question"
-            value={this.props.content}
-            className="w-100 h pa2 bn foucs-border-green hover-border"
-            onChange = {(event) => this.props.handleQuestionChange({ event, questionId:this.props.id })}
+  return (
+    <div 
+      className="fl w-100 bg-light-green2 bt bw3 border-dark-green2 mt4"
+      onClick={() => handleQuesionComponentClick(index)}
+      onBlur={()=> handleUpdateQuestion()}
+    >
+      <div className="fl f5 code pa2 tr pl2 mt3">
+        {index + 1}.
+      </div>
+      <div className="center w-90 relative mt3">
+        <input
+          name='question'
+          type='text'
+          placeholder=" question"
+          value={props.content}
+          className="w-100 h pa2 bn foucs-border-green hover-border"
+          onChange = {(event) => props.handleQuestionChange({ event, questionId: props.id })}
+        />
+        {answers.map(answer => (
+          <EditQuestionAnswear 
+            key={answer.id}
+            answearContent={answer.content}
+            answerId={answer.id}
+            questionId={props.id}
           />
-          {answers.map(answer => (
-            <EditQuestionAnswear 
-              key={answer.id}
-              answearContent={answer.content}
-              answerId={answer.id}
-              questionId={this.props.id}
-            />
-            ))}
+          ))}
 
-        </div>
-        <div className="fl w-100"></div>
-        <div onClick = {this.handleAddAnswear} className="fl pl3 pointer dim">
-          <div className='flex items-center'>
-            <div className="fl f2 pa2 b font-light-green3 dim">&#43;</div>
-            <div className="fl black-70 f4 mt1">Add options</div>
-          </div>
+      </div>
+      <div className="fl w-100"></div>
+      <div onClick = {handleAddAnswear} className="fl pl3 pointer dim">
+        <div className='flex items-center'>
+          <div className="fl f2 pa2 b font-light-green3 dim">&#43;</div>
+          <div className="fl black-70 f4 mt1">Add options</div>
         </div>
       </div>
-    )
-  }
-}
-
-
+    </div>
+  )  
+};
 const mapDispatchToProps = dispatch => {
   return {
     updateQuestion: content => dispatch(UPDATE_QUESTION(content)),
