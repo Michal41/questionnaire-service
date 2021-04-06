8c7e785a2f5187218221d573aa30078f1f578d07
michal <michal@michals-MacBook-Pro.local>
2021-04-06 21:48:35 +0200

    Add status to questionnaires

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_30_162102) do
+ActiveRecord::Schema.define(version: 2021_04_06_194409) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2021_03_30_162102) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "description"
     t.integer "user_id"
+    t.integer "status", default: 0
   end
 
   create_table "questions", force: :cascade do |t|
